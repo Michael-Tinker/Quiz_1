6a60eeb943cb5c022aacbc93d2103523c0a580e2
Michael-Tinker <michaelt19@protonmail.com>
2022-02-14 15:24:18 -0600

    Completed

--- processStudents.py
+++ processStudents.py
@@ -24,55 +24,59 @@ import csv
 
 
 # create a file object to open the file in read mode
-
-
+students = open("students.csv","r")
 
 # create a csv object from the file object
-
+student_file = csv.reader(students, delimiter=",")
 
 #skip the header row
-
+next(student_file)
 
 #create an outfile object for the pocessed record
+CSVFILE = "processedStudents.csv"
+outfile = open(CSVFILE, "w", newline="")
+writer = csv.writer(outfile, delimiter=",")
 
+fieldnames = ["stud_id", "firstname", "lastname", "major", "classification", "gpa"]
+writer.writerow(fieldnames)
 
 
 #create a new dictionary named 'student_dict'
-
-
+student_dict = {}
 
 #use a loop to iterate through each row of the file
-
+for record in student_file:
     #check if the GPA is below 3.0. If so, write the record to the outfile
-    
-        
-
-
-
+    if float(record[8]) < 3.0:
+        stud_id = record[0]
+        pin = record[1]
+        firstname = record[2]
+        lastname = record[3]
+        city = record[4]
+        state = record[5]
+        major = record[6]
+        classification = record[7]
+        gpa = record[8]
+        row = [stud_id, firstname, lastname, major, classification, gpa]
+        writer.writerow(row)
     # append the record to the dictionary with the student id as the Key
     # and the value as the GPA
+    key = record[0]
+    student_dict[key] = record[8]
     
 
-
-
-
-
 #print the entire dictionary
-
+print(student_dict)
 
 #Print the student id 
-
+id = "567890123"
+print(id)
 
 #print out the corresponding GPA from the dictionary
-
-
+if id in student_dict:
+    print(student_dict[id])
+else:
+    print(id, "is not in the dictionary student_dict")
 
 #close the outfile
-
-
-
-
-
-
-
-
+outfile.close()
